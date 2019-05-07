614fb64e954369da438e42923179caedfa067904
Ivica Sirjan <isirjan@gmail.com>
2019-05-07 17:18:16 +0200

    Add connection credentials

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,10 @@
-const { Pool } = require('pg');
+const { Pool } = require('pg'); // { Pool } je rastavljanje/destrukturiranje objekta, uzimamo samo stvari koje nam trebaju
 
 const pool = new Pool({
-  user: '', //marinokol
-  host: '', //138.68.87.73
-  database: '', // db_marinokol
-  password: '', // your password
+  user: 'ivicasi', //marinokol
+  host: '138.68.87.73', //138.68.87.73
+  database: 'db_ivicasi', // db_marinokol
+  password: 'GVK?4N>9', // your password
   port: 5432, // 5432
 })
 
